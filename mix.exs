6e01069877611bb0b3a5a2af4e36b0f376224996
RaymondLoranger <Raymond.Loranger@bell.net>
2020-04-07 09:39:27 -0400

    0.1.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Tally.MixProject do
   def project do
     [
       app: :islands_tally,
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands Tally",
